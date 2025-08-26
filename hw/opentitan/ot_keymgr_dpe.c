43b05367b8cb510f9ceec0e01659462e9740ff37
Alex Jones <alex.jones@lowrisc.org>
2025-08-26 17:25:06 +0100

    [ot] hw/opentitan: ot_keymgr_dpe: Small clean up
    Clean up some parts of the keymgr_dpe code, breaking up a couple of more
    complicated compound expressions to make them more readable, and
    removing unnecessary compounds and branching.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- hw/opentitan/ot_keymgr_dpe.c
+++ hw/opentitan/ot_keymgr_dpe.c
@@ -531,7 +531,6 @@ static const char *WORKING_STATE_NAMES[] = {
     WORKING_STATE_ENTRY(INVALID),
 };
 #undef WORKING_STATE_ENTRY
-
 #define WORKING_STATE_NAME(_st_) \
     (((unsigned)(_st_)) < ARRAY_SIZE(WORKING_STATE_NAMES) ? \
          WORKING_STATE_NAMES[(_st_)] : \
@@ -718,12 +717,10 @@ static void ot_keymgr_dpe_push_entropy(void *opaque, uint32_t bits, bool fips)
     if (resched) {
         /* We need more entropy */
         ot_keymgr_dpe_request_entropy(s);
-    } else {
-        if (prng->reseed_req) {
-            prng->reseed_ack = true;
-            prng->reseed_cnt = 0u;
-            ot_keymgr_dpe_schedule_fsm(s);
-        }
+    } else if (prng->reseed_req) {
+        prng->reseed_ack = true;
+        prng->reseed_cnt = 0u;
+        ot_keymgr_dpe_schedule_fsm(s);
     }
 }
 
@@ -1769,9 +1766,8 @@ static void ot_keymgr_dpe_write(void *opaque, hwaddr addr, uint64_t val64,
         val32 &= R_CONTROL_SHADOWED_MASK;
         switch (ot_shadow_reg_write(&s->control, val32)) {
         case OT_SHADOW_REG_STAGED:
-        case OT_SHADOW_REG_COMMITTED: {
+        case OT_SHADOW_REG_COMMITTED:
             break;
-        }
         case OT_SHADOW_REG_ERROR:
         default:
             s->regs[R_ERR_CODE] |= R_ERR_CODE_INVALID_SHADOW_UPDATE_MASK;
@@ -1826,13 +1822,14 @@ static void ot_keymgr_dpe_write(void *opaque, hwaddr addr, uint64_t val64,
     case R_SW_BINDING_4:
     case R_SW_BINDING_5:
     case R_SW_BINDING_6:
-    case R_SW_BINDING_7:
+    case R_SW_BINDING_7: {
         if (!ot_keymgr_dpe_check_reg_write(s, reg, R_SW_BINDING_REGWEN)) {
             break;
         }
-        stl_le_p(&s->sw_binding[(reg - R_SW_BINDING_0) * sizeof(uint32_t)],
-                 val32);
+        unsigned offset = (reg - R_SW_BINDING_0) * sizeof(uint32_t);
+        stl_le_p(&s->sw_binding[offset], val32);
         break;
+    }
     case R_SALT_0:
     case R_SALT_1:
     case R_SALT_2:
@@ -1840,12 +1837,14 @@ static void ot_keymgr_dpe_write(void *opaque, hwaddr addr, uint64_t val64,
     case R_SALT_4:
     case R_SALT_5:
     case R_SALT_6:
-    case R_SALT_7:
+    case R_SALT_7: {
         if (!ot_keymgr_dpe_check_reg_write(s, reg, R_CFG_REGWEN)) {
             break;
         }
-        stl_le_p(&s->salt[(reg - R_SALT_0) * sizeof(uint32_t)], val32);
+        unsigned offset = (reg - R_SALT_0) * sizeof(uint32_t);
+        stl_le_p(&s->salt[offset], val32);
         break;
+    }
     case R_KEY_VERSION:
         if (!ot_keymgr_dpe_check_reg_write(s, reg, R_CFG_REGWEN)) {
             break;
