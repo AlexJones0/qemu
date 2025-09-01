1440cc26cfd88862da35079403e71492a43bcd14
Alex Jones <alex.jones@lowrisc.org>
2025-09-01 19:42:40 +0100

    [ot] docs/opentitan: Update Keymgr status in Earlgrey docs
    Also document the final known limitations and TODOs for the `keymgr` in
    the prologue.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- hw/opentitan/ot_keymgr.c
+++ hw/opentitan/ot_keymgr.c
@@ -24,6 +24,15 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
+ * Known limitations:
+ *  - Entropy reseeding not currently supported: the value in the shadowed
+ *  `RESEED_INTERVAL_THRESHOLD` register is ignored.
+ *  - Currently the keymgr is designed without KMAC masking. If KMAC masking
+ *    is enabled, a small amount of logic (loading creator root key and
+ *    KMAC data / key integrity checks) needs to be updated to use both key
+ *    shares correctly.
+ *  - Some faults may not be modelled completely accurately (i.e., untested).
  */
 
 #include "qemu/osdep.h"
